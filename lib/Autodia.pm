3111db11130b762bae0593b99ed4c1d676d93ed3
aaron.trevena <aaron.trevena@e623d1f2-fd4a-0410-b252-e5105326df6d>
2011-05-23 12:20:37 +0000

    fixed MANIFEST.. now 2.12
    git-svn-id: http://autodia.googlecode.com/svn/trunk@73 e623d1f2-fd4a-0410-b252-e5105326df6d

--- lib/Autodia.pm
+++ lib/Autodia.pm
@@ -7,7 +7,7 @@ Autodia.pm - The configuration and Utility perl module for AutoDia.
 
 =head1 VERSION
 
-2.11
+2.12
 
 =head1 DESCRIPTION
 
@@ -75,7 +75,7 @@ my %perl = (
 BEGIN {
         use Exporter ();
         use vars qw($VERSION @ISA @EXPORT);
-        $VERSION = "2.11";
+        $VERSION = "2.12";
         @ISA = qw(Exporter);
         @EXPORT = qw(
 		     &getHandlers
